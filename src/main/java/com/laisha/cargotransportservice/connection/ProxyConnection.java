4cd2e3bf6403cbbca25242d4cf03bfdec0a0a863
kiryl-laisha <laishakaya@gmail.com>
2022-04-20 08:45:05 +0300

    changed access modifier.

--- src/main/java/com/laisha/cargotransportservice/connection/ProxyConnection.java
+++ src/main/java/com/laisha/cargotransportservice/connection/ProxyConnection.java
@@ -12,7 +12,7 @@ import java.util.concurrent.Executor;
 public class ProxyConnection implements Connection {
 
     private static final Logger logger = LogManager.getLogger();
-    Connection connection;
+    private final Connection connection;
 
     public ProxyConnection(Connection connection) {
         this.connection = connection;
